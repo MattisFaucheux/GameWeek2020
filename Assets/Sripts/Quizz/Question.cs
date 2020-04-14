66201ae154128bc7d2010dbccf41589c9f320d2f
Mattis Faucheux <mattis.faucheux@gmail.com>
2020-04-14 17:47:39 -0400

    Add Quizz, Player & Timer

---
+++ Assets/Sripts/Quizz/Question.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Serialization;
+
+public class Question
+{
+    [XmlAttribute("name")] public string Name;
+    [XmlAttribute("answer1")] public string Answer1;
+    [XmlAttribute("answer2")] public string Answer2;
+    [XmlAttribute("answer3")] public string Answer3;
+    [XmlAttribute("answer4")] public string Answer4;
+    [XmlAttribute("answer")] public int AnswerIndex;
+}
