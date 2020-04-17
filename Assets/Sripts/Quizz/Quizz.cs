349e97494bb21dd19e1aa62a1c76ed3eb0c82041
Mattis Faucheux <mattis.faucheux@gmail.com>
2020-04-16 22:41:57 -0400

    test repair

--- Assets/Sripts/Quizz/Quizz.cs
+++ Assets/Sripts/Quizz/Quizz.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Xml.Serialization;
 using System.IO;
+using System.Xml;
 
 [XmlRoot("Questions")]
 public class Quizz
@@ -15,10 +16,20 @@ public class Quizz
     public static Quizz LoadFromFile(string filepath)
     {
         XmlSerializer serializer = new XmlSerializer(typeof(Quizz));
-        using (FileStream stream = new FileStream(filepath, FileMode.Open))
+
+        string p_path;
+        p_path = Path.Combine(Application.dataPath, filepath);
+
+        using (FileStream stream = new FileStream(p_path, FileMode.Open))
         {
             return serializer.Deserialize(stream) as Quizz;
         }
 
     }
 }
+
+
+
+//TextAsset temp = Resources.Load(filepath) as TextAsset;
+//XmlDocument _doc = new XmlDocument();
+//_doc.LoadXml(temp.text);
