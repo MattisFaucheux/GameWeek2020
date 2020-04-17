eb2db1fd0e01e5b97caa50d6083f929e53610257
Mattis Faucheux <mattis.faucheux@gmail.com>
2020-04-16 22:42:31 -0400

    Revert "Merge branch 'MattisProg'"
    This reverts commit 1c0d9b0e2737201fb2d6345d31411616588dd749.

--- Assets/Sripts/Quizz/Quizz.cs
+++ Assets/Sripts/Quizz/Quizz.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Xml.Serialization;
 using System.IO;
-using System.Xml;
 
 [XmlRoot("Questions")]
 public class Quizz
@@ -16,20 +15,10 @@ public class Quizz
     public static Quizz LoadFromFile(string filepath)
     {
         XmlSerializer serializer = new XmlSerializer(typeof(Quizz));
-
-        string p_path;
-        p_path = Path.Combine(Application.dataPath, filepath);
-
-        using (FileStream stream = new FileStream(p_path, FileMode.Open))
+        using (FileStream stream = new FileStream(filepath, FileMode.Open))
         {
             return serializer.Deserialize(stream) as Quizz;
         }
 
     }
 }
-
-
-
-//TextAsset temp = Resources.Load(filepath) as TextAsset;
-//XmlDocument _doc = new XmlDocument();
-//_doc.LoadXml(temp.text);
